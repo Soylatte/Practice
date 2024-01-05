47b9b0af026b4200723d9d70ed21fe7078a3718d
Soyeon Kim <kksoyeon61@gmail.com>
2024-01-05 11:59:10 +0100

    build own todo

---
+++ practice/src/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import TodoListTemplate from "./Component/TodoListTemplate";
+
+
+class App extends Component{
+  render() {
+    return(
+      <TodoListTemplate>완성</TodoListTemplate>
+    )
+  }
+}
+export default App;
